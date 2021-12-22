d9e66888d41cb57c8155d7f6c5244bf48e06c624
Glenn Boonen <r0242802@student.thomasmore.be>
2021-12-22 21:13:38 +0100

    project2 upload
    first upload of project 2 source files

---
+++ vendor/multipleCardCarousel.js
@@ -0,0 +1,34 @@
+var multipleCardCarousel = document.querySelector(
+    "#carouselCVControls"
+);
+
+if (window.matchMedia("(min-width: 768px)").matches) {
+    var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+        interval: false,
+    });
+    var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+    var cardWidth = $(".carousel-item").width();
+    var scrollPosition = 0;
+
+    $("#carouselCVControls .carousel-control-next").on("click", function () {
+        if (scrollPosition < carouselWidth - cardWidth * 4) {
+            scrollPosition += cardWidth;
+            $("#carouselCVControls .carousel-inner").animate(
+                { scrollLeft: scrollPosition },
+                600
+            );
+        }
+    });
+
+    $("#carouselCVControls .carousel-control-prev").on("click", function () {
+        if (scrollPosition > 0) {
+            scrollPosition -= cardWidth;
+            $("#carouselCVControls .carousel-inner").animate(
+                { scrollLeft: scrollPosition },
+                600
+            );
+        }
+    });
+} else {
+    $(multipleCardCarousel).addClass("slide");
+}
